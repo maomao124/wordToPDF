949dc51288351ea2c3799f266e800d7710421932
mao <1296193245@qq.com>
2023-06-16 13:43:25 +0800

    wordToPDF

---
+++ src/main/java/mao/wordtopdf/WordToPdfApplication.java
@@ -0,0 +1,15 @@
+package mao.wordtopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordToPdfApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WordToPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/wordtopdf/WordToPdfApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.wordtopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordToPdfApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
